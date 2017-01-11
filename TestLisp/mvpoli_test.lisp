9a1c863aac5445ddce158178c4e449d84773b1ae
mpolonioli <m.polonioli@campus.unimib.it>
2017-01-11 18:42:35 +0100

    aggiunte cartelle e file di test

---
+++ TestLisp/mvpoli_test.lisp
@@ -0,0 +1,535 @@
+;;;; Unit testing file for mvpoli library
+
+;;;; Place in the same directory of mvpoli.lisp file,
+;;;; then execute/load this file in order to test project required functionalities.
+
+(defparameter *monomial-parsing-ok* 0)
+
+(defparameter *monomial-parsing-errors* 0)
+
+(defparameter *polynomial-parsing-ok* 0)
+
+(defparameter *polynomial-parsing-errors* 0)
+
+(defparameter *utility-functions-ok* 0)
+
+(defparameter *utility-functions-errors* 0)
+
+(defparameter *arithmetic-functions-ok* 0)
+
+(defparameter *arithmetic-functions-errors* 0)
+
+(defun update-test (&key category result)
+  (cond ((eql category 'monomial-parsing)
+	 (if result
+	     (defparameter *monomial-parsing-ok*
+	       (1+ *monomial-parsing-ok*))
+	     (defparameter *monomial-parsing-errors*
+	       (1+ *monomial-parsing-errors*))))
+
+	((eql category 'polynomial-parsing)
+	 (if result
+	     (defparameter *polynomial-parsing-ok*
+	       (1+ *polynomial-parsing-ok*))
+	     (defparameter *polynomial-parsing-errors*
+	       (1+ *polynomial-parsing-errors*))))
+
+	((eql category 'utility-functions)
+	 (if result
+	     (defparameter *utility-functions-ok*
+	       (1+ *utility-functions-ok*))
+	     (defparameter *utility-functions-errors*
+	       (1+ *utility-functions-errors*))))
+
+	((eql category 'arithmetic-functions)
+	 (if result
+	     (defparameter *arithmetic-functions-ok*
+	       (1+ *arithmetic-functions-ok*))
+	     (defparameter *arithmetic-functions-errors*
+	       (1+ *arithmetic-functions-errors*))))))
+
+(defun test (name)
+  (let ((*default-pathname-defaults* (make-pathname :defaults *load-truename*)))
+    (load name)))
+
+(defun test-function (&key result input1 input2 function category)
+  (let* ((expected result)
+	 (expression1 input1)
+	 (expression2 input2)
+	 (parsed (if (null expression2)
+		     (funcall function expression1)
+		     (funcall function expression1 expression2)))
+	 (res (equal expected parsed))
+	 (update (update-test :category category
+			      :result res)))
+    (if res
+	(format t "Ok")
+	(format t "Failed, expected: ~s, got: ~s" expected parsed))))
+
+(defun test-as-monomial ()
+  (test-function :result '(M 3 5 ((V 2 X) (V 3 Y)))
+		 :input1 '(* 3 (expt x 2) (expt y 3))
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-with-only-functor-symbol ()
+  (test-function :result '(M 1 0 NIL)
+		 :input1 '(*)
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-without-coefficient ()
+  (test-function :result '(M 1 5 ((V 2 X) (V 3 Y)))
+		 :input1 '(* (expt x 2) (expt y 3))
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-expressed-by-only-a-number ()
+  (test-function :result '(M 3 0 NIL)
+		 :input1 '3
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-with-variable-expressed-only-by-a-symbol ()
+  (test-function :result '(M 3 1 ((V 1 X)))
+		 :input1 '(* 3 x)
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-with-variable-expressed-by-several-symbols ()
+  (test-function :result '(M 3 2 ((V 2 TEST)))
+		 :input1 '(* 3 (expt test 2))
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-variables-reduction ()
+  (test-function :result '(M 3 3 ((V 3 Y)))
+		 :input1 '(* 3 (expt x 0) (expt y 3))
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-monomial-variables-minimization ()
+  (test-function :result '(M 3 5 ((V 5 X)))
+		 :input1 '(* 3 (expt x 2) (expt x 3))
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-monomial-sorting ()
+  (test-function :result '(M 3 5 ((V 2 X) (V 3 Y)))
+		 :input1 '(* 3 (expt y 3) (expt x 2))
+		 :function #'as-monomial
+		 :category 'monomial-parsing))
+
+(defun test-as-polynomial ()
+  (test-function :result '(POLY ((M 3 5 ((V 2 X) (V 3 Y)))))
+		 :input1 '(+ (* 3 (expt y 3) (expt x 2)))
+		 :function #'as-polynomial
+		 :category 'polynomial-parsing))
+
+(defun test-as-polynomial-of-monomial-expression ()
+  (test-function :result '(POLY ((M 2 0 NIL)))
+		 :input1 '(* 2)
+		 :function #'as-polynomial
+		 :category 'polynomial-parsing))
+
+(defun test-as-polynomial-expressed-only-by-a-number ()
+  (test-function :result '(POLY ((M 2 0 NIL)))
+		 :input1 '2
+		 :function #'as-polynomial
+		 :category 'polynomial-parsing))
+
+(defun test-as-polynomial-with-null-monomial ()
+  (test-function :result '(POLY NIL)
+		 :input1 '0
+		 :function #'as-polynomial
+		 :category 'polynomial-parsing))
+
+(defun test-polynomial-sorting ()
+  (test-function :result '(POLY ((M 1 1 ((V 1 X)))
+				 (M -2 2 ((V 1 X) (V 1 Y)))
+				 (M 1 2 ((V 2 X)))))
+		 :input1 '(+ (* (expt x 2)) (* x) (* -2 y x))
+		 :function #'as-polynomial
+		 :category 'polynomial-parsing))
+
+(defun test-varpowers ()
+  (test-function :result '((V 1 X) (V 1 Y))
+		 :input1 '(M 1 2 ((V 1 X) (V 1 Y)))
+		 :function #'varpowers
+		 :category 'utility-functions))
+
+(defun test-varpowers-with-no-variables ()
+  (test-function :result 'NIL
+		 :input1 '(* 0 x)
+		 :function #'varpowers
+		 :category 'utility-functions))
+
+(defun test-vars-of ()
+  (test-function :result '(X Y)
+		 :input1 '(* x y)
+		 :function #'vars-of
+		 :category 'utility-functions))
+
+(defun test-vars-of-with-no-variables ()
+  (test-function :result 'NIL
+		 :input1 '(* 0 x)
+		 :function #'vars-of
+		 :category 'utility-functions))
+
+(defun test-monomial-degree ()
+  (test-function :result '3
+		 :input1 '(* 3 x (expt y 2))
+		 :function #'monomial-degree
+		 :category 'utility-functions))
+
+(defun test-null-monomial-degree ()
+  (test-function :result '0
+		 :input1 '(* 3)
+		 :function #'monomial-degree
+		 :category 'utility-functions))
+
+(defun test-monomial-coefficient ()
+  (test-function :result '1
+		 :input1 '(* y z)
+		 :function #'monomial-coefficient
+		 :category 'utility-functions))
+
+(defun test-null-monomial-coefficient ()
+  (test-function :result '0
+		 :input1 '(* 0 y z)
+		 :function #'monomial-coefficient
+		 :category 'utility-functions))
+
+(defun test-coefficients ()
+  (test-function :result '(-2 3 1)
+		 :input1 '(+ (* 3 x) -2 (* y))
+		 :function #'coefficients
+		 :category 'utility-functions))
+
+(defun test-coefficients-of-null-polynomial ()
+  (test-function :result '(0)
+		 :input1 '(+ (* -3 x) (* 3 x))
+		 :function #'coefficients
+		 :category 'utility-functions))
+
+(defun test-variables ()
+  (test-function :result '(x y z)
+		 :input1 '(+ (* 3 x) -2 (* y z))
+		 :function #'variables
+		 :category 'utility-functions))
+
+(defun test-monomials ()
+  (test-function :result '((M 1 0 NIL)
+			   (M 1 3 ((V 1 X) (V 1 Y) (V 1 Z)))
+			   (M -1 3 ((V 1 X) (V 2 Y))))
+		 :input1 '(+ (* x y z) 1 (* -1 x (expt y 2)))
+		 :function #'monomials
+		 :category 'utility-functions))
+
+(defun test-maxdegree ()
+  (test-function :result '3
+		 :input1 '(+ 4 (* x y z) (* x y))
+		 :function #'maxdegree
+		 :category 'utility-functions))
+
+(defun test-mindegree ()
+  (test-function :result '0
+		 :input1 '(+ 4 (* x y z) (* x y))
+		 :function #'mindegree
+		 :category 'utility-functions))
+
+(defun test-polyplus ()
+  (test-function :result '(POLY ((M 2 0 NIL)
+				 (M 1 1 ((V 1 X)))
+				 (M 2 3 ((V 2 X) (V 1 Y)))))
+		 :input1 '(+ -1 (* x x y))
+		 :input2 '(+ 3 (* x) (* x x y))
+		 :function #'polyplus
+		 :category 'arithmetic-functions))
+
+(defun test-polyminus ()
+  (test-function :result '(POLY ((M -4 0 NIL)
+				 (M -1 1 ((V 1 X)))))
+		 :input1 '(+ -1 (* x x y))
+		 :input2 '(+ 3 (* x) (* x x y))
+		 :function #'polyminus
+		 :category 'arithmetic-functions))
+
+(defun test-polyplus-0 ()
+  (test-function :result '(POLY NIL)
+		 :input1 '(+ (* -1 x x y))
+		 :input2 '(+ (* x x y))
+		 :function #'polyplus
+		 :category 'arithmetic-functions))
+
+(defun test-polyminus-0 ()
+  (test-function :result '(POLY NIL)
+		 :input1 '(+ -1 (* x x y))
+		 :input2 '(+ -1 (* x x y))
+		 :function #'polyminus
+		 :category 'arithmetic-functions))
+
+(defun test-polyplus-with-first-argument-0 ()
+  (test-function :result '(POLY ((M 3 0 NIL)))
+		 :input1 '0
+		 :input2 '(+ 3)
+		 :function #'polyplus
+		 :category 'arithmetic-functions))
+
+(defun test-polyplus-with-second-argument-0 ()
+  (test-function :result '(POLY ((M 3 0 NIL)))
+		 :input1 '(+ 3)
+		 :input2 '(POLY NIL)
+		 :function #'polyplus
+		 :category 'arithmetic-functions))
+
+(defun test-polyminus-with-first-argument-0 ()
+  (test-function :result '(POLY ((M -3 0 NIL)))
+		 :input1 '0
+		 :input2 '(+ 3)
+		 :function #'polyminus
+		 :category 'arithmetic-functions))
+
+(defun test-polyminus-with-second-argument-0 ()
+  (test-function :result '(POLY ((M 3 0 NIL)))
+		 :input1 '(+ 3)
+		 :input2 '(POLY NIL)
+		 :function #'polyminus
+		 :category 'arithmetic-functions))
+
+(defun test-polytimes ()
+  (test-function :result '(POLY ((M 6 1 ((V 1 X)))
+				 (M 2 3 ((V 2 X) (V 1 Y)))))
+		 :input1 '(+ (* -2 x))
+		 :input2 '(+ -3 (* -1 x y))
+		 :function #'polytimes
+		 :category 'arithmetic-functions))
+
+(defun test-polytimes-minimization ()
+  (test-function :result '(POLY ((M -1 0 NIL) (M 1 2 ((V 2 X)))))
+		 :input1 '(+ (* x) 1)
+		 :input2 '(+ (* x) -1)
+		 :function #'polytimes
+		 :category 'arithmetic-functions))
+
+(defun test-polytimes-with-first-argument-0 ()
+  (test-function :result '(POLY NIL)
+		 :input1 '0
+		 :input2 '(+ 3)
+		 :function #'polytimes
+		 :category 'arithmetic-functions))
+
+(defun test-polytimes-with-second-argument-0 ()
+  (test-function :result '(POLY NIL)
+		 :input1 '(+ 3)
+		 :input2 '(POLY NIL)
+		 :function #'polytimes
+		 :category 'arithmetic-functions))
+
+(defun test-polytimes-with-first-argument-1 ()
+  (test-function :result '(POLY ((M 3 0 NIL)
+				 (M 1 1 ((V 1 X)))))
+		 :input1 '1
+		 :input2 '(+ (* x) 3)
+		 :function #'polytimes))
+
+(defun test-polytimes-with-second-argument-1 ()
+  (test-function :result '(POLY ((M 3 0 NIL)
+				 (M 1 1 ((V 1 X)))))
+		 :input1 '(+ (* x) 3)
+		 :input2 '1
+		 :function #'polytimes
+		 :category 'arithmetic-functions))
+
+(defun test-polyval ()
+  (test-function :result '16
+		 :input1 '(+ (* x (expt y 3)) (* 0 z))
+		 :input2 '(2 2)
+		 :function #'polyval
+		 :category 'arithmetic-functions))
+
+(defun test-polyval-minimization ()
+  (test-function :result '0
+		 :input1 '(+ (* x) (* -1 x))
+		 :input2 '(1 2)
+		 :function #'polyval
+		 :category 'arithmetic-functions))
+
+(defun test-polyval-with-more-variable-values ()
+  (test-function :result '16
+		 :input1 '(+ (* x (expt y 3)) (* 0 z))
+		 :input2 '(2 2 3)
+		 :function #'polyval
+		 :category 'arithmetic-functions))
+
+(test "mvpoli")
+
+(format t "~&~%-- Begin tests --~%~%-- Monomial parsing tests--")
+
+(format t "~&Testing monomial parsing...")
+(test-as-monomial)
+
+(format t "~&Testing monomial parsing represented by only functor symbol...")
+(test-as-monomial-with-only-functor-symbol)
+
+(format t "~&Testing monomial parsing without coefficient...")
+(test-as-monomial-without-coefficient)
+
+(format t "~&Testing monomial parsing represented only by a number...")
+(test-as-monomial-expressed-by-only-a-number)
+
+(format t "~&Testing monomial parsing with a variable expressed only by a symbol...")
+(test-as-monomial-with-variable-expressed-only-by-a-symbol)
+
+(format t "~&Testing monomial parsing with a variable expressed by several symbols...")
+(test-as-monomial-with-variable-expressed-by-several-symbols)
+
+(format t "~&Testing monomial parsing with 0 as variable power...")
+(test-as-monomial-variables-reduction)
+
+(format t "~&Testing monomial parsing using variables with same symbol...")
+(test-as-monomial-variables-minimization)
+
+(format t "~&Testing monomial sorting...")
+(test-monomial-sorting)
+
+(format t "~&~%-- Polynomial parsing tests --")
+
+(format t "~&Testing polynomial parsing...")
+(test-as-polynomial)
+
+(format t "~&Testing polynomial parsing of monomial expression...")
+(test-as-polynomial-of-monomial-expression)
+
+(format t "~&Testing polynomial parsing of number expression...")
+(test-as-polynomial-expressed-only-by-a-number)
+
+(format t "~&Testing null polynomial parsing...")
+(test-as-polynomial-with-null-monomial)
+
+(format t "~&Testing polynomial sorting...")
+(test-polynomial-sorting)
+
+(format t "~&~%-- Utility functions tests --")
+
+(format t "~&Testing varpowers...")
+(test-varpowers)
+
+(format t "~&Testing varpowers with no variables...")
+(test-varpowers-with-no-variables)
+
+(format t "~&Testing vars-of...")
+(test-vars-of)
+
+(format t "~&Testing vars-of with no variables...")
+(test-vars-of-with-no-variables)
+
+(format t "~&Testing monomial-degree...")
+(test-monomial-degree)
+
+(format t "~&Testing null monomial-degree...")
+(test-null-monomial-degree)
+
+(format t "~&Testing monomial-coefficient...")
+(test-monomial-coefficient)
+
+(format t "~&Testing null monomial-coefficient...")
+(test-null-monomial-coefficient)
+
+(format t "~&Testing coefficients...")
+(test-coefficients)
+
+(format t "~&Testing coefficients of null polynomial...")
+(test-coefficients-of-null-polynomial)
+
+(format t "~&Testing variables...")
+(test-variables)
+
+(format t "~&Testing monomials...")
+(test-monomials)
+
+(format t "~&Testing maxdegree...")
+(test-maxdegree)
+
+(format t "~&Testing mindegree...")
+(test-mindegree)
+
+(format t "~&~%-- Arithmetic functions tests --")
+
+(format t "~&Testing polyplus...")
+(test-polyplus)
+
+(format t "~&Testing polyminus...")
+(test-polyminus)
+
+(format t "~&Testing polyplus with 0 as result...")
+(test-polyplus-0)
+
+(format t "~&Testing polyminus with 0 as result...")
+(test-polyminus-0)
+
+(format t "~&Testing polyplus using null polynomial as first value...")
+(test-polyplus-with-first-argument-0)
+
+(format t "~&Testing polyplus using null polynomial as second value...")
+(test-polyplus-with-second-argument-0)
+
+(format t "~&Testing polyminus using null polynomial as first value...")
+(test-polyminus-with-first-argument-0)
+
+(format t "~&Testing polyminus using null polynomial as second value...")
+(test-polyminus-with-second-argument-0)
+
+(format t "~&Testing polytimes...")
+(test-polytimes)
+
+(format t "~&Testing polytimes minimimization...")
+(test-polytimes-minimization)
+
+(format t "~&Testing polytimes using null polynomial as first value...")
+(test-polytimes-with-first-argument-0)
+
+(format t "~&Testing polytimes using null polynomial as secondfirst value...")
+(test-polytimes-with-second-argument-0)
+
+(format t "~&Testing polytimes using neutral polynomial as first value...")
+(test-polytimes-with-first-argument-1)
+
+(format t "~&Testing polytimes using neutral polynomial as secondfirst value...")
+(test-polytimes-with-second-argument-1)
+
+(format t "~&Testing polyval...")
+(test-polyval)
+
+(format t "~&Testing polyval minimization...")
+(test-polyval-minimization)
+
+(format t "~&Testing polyval with more variable values then variables...")
+(test-polyval-with-more-variable-values)
+
+(format t
+	"~&~%Tests result, passed: ~d, failed: ~d"
+	(+ *monomial-parsing-ok* *polynomial-parsing-ok*
+	   *utility-functions-ok* *arithmetic-functions-ok*)
+	(+ *monomial-parsing-errors* *polynomial-parsing-errors*
+	   *utility-functions-errors* *arithmetic-functions-errors*))
+
+(format t
+	"~&Monomial parsing tests, passed: ~d, failed: ~d"
+	*monomial-parsing-ok*
+	*monomial-parsing-errors*)
+
+(format t
+	"~&Polynomial parsing tests, passed: ~d, failed: ~d"
+	*polynomial-parsing-ok*
+	*polynomial-parsing-errors*)
+
+(format t
+	"~&Utility functions tests, passed: ~d, failed: ~d"
+	*utility-functions-ok*
+	*utility-functions-errors*)
+
+(format t
+	"~&Arithmetic functions tests, passed: ~d, failed: ~d"
+	*arithmetic-functions-ok*
+	*arithmetic-functions-errors*)
---
+++ TestProlog/mvpoli.plt
@@ -0,0 +1,92 @@
+/*
+?- consult('mvpoli.pl').
+?- load_test_files([]), run_tests.
+*/
+:- begin_tests(mvpoli).
+%------------------------------------------------------------------------------
+test(is_varpower_1) :- mvpoli:is_varpower(v(1,x)).
+test(is_varpower_2) :- not(mvpoli:is_varpower(v(x,1))).
+test(is_varpower_3) :- not(mvpoli:is_varpower(v(-1,x))).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(is_monomial_1) :- mvpoli:is_monomial(m(4, 1, [v(1, x)])).
+test(is_monomial_2) :- mvpoli:is_monomial(m(1, 1, [v(1, x)])).
+test(is_monomial_3) :- mvpoli:is_monomial(m(1, 0, [])).
+test(is_monomial_4) :- not(mvpoli:is_monomial(poly([m(1, 0, [])]))).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(is_polynomial_1) :- not(mvpoli:is_polynomial(m(4, 1, [v(1, x)]))).
+test(is_polynomial_2) :- mvpoli:is_polynomial(poly([m(1, 0, [])])).
+test(is_polynomial_3) :- mvpoli:is_polynomial(poly([m(4, 2, [v(1, x), v(1, y)]), m(1, 2, [v(2, x)]), m(5, 2, [v(2, y)])])).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(coefficients_1) :- mvpoli:coefficients(poly([m(42, 0, [])]), [42]).
+test(coefficients_2) :-	mvpoli:coefficients(poly([m(3, 1, [v(1, x)]), m(42, 0, [])]), [42, 3]).
+test(coefficients_3) :-	mvpoli:coefficients(poly([m(3, 1, [v(1, x)]), m(-4, 1, [v(1, x)]), m(1, 0, [])]), [1, -1]).
+test(coefficients_4) :-	mvpoli:coefficients(poly([m(-4, 2, [v(2, x)]), m(3, 1, [v(1, x)]), m(1, 0, [])]), [1 , 3, -4]).
+test(coefficients_5) :-	mvpoli:coefficients(poly([m(1, 8, [v(2, x), v(5, y), v(1, z)]), m(1, 5, [v(1, x), v(1, y), v(3, z)]), m(1, 3, [v(1, x), v(1, y), v(1, z)])]), [1, 1, 1]).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(variables_1) :- mvpoli:variables(poly([m(42, 0, [])]), []).
+test(variables_2) :- mvpoli:variables(poly([m(3, 1, [v(1, x)]),m(42, 0, [])]), [x]).
+test(variables_3) :- mvpoli:variables(poly([m(3, 1, [v(1, x)]), m(-4, 1, [v(1, x)]), m(1, 0, [])]), [x]).
+test(variables_4) :- mvpoli:variables(poly([m(-4, 2, [v(2, x)]), m(3, 1, [v(1, x)]), m(1, 0, [])]), [x]).
+test(variables_5) :- mvpoli:variables(poly([m(1, 8, [v(2, x), v(5, y), v(1, z)]), m(1, 5, [v(1, x), v(1, y), v(3, z)]), m(1, 3, [v(1, x), v(1, y), v(1, z)])]), [x, y, z]).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(monomials_1) :- mvpoli:monomials(poly([m(1,1,[v(1,b)]), m(1,1,[v(1,a)])]), [m(1, 1, [v(1, a)]), m(1, 1, [v(1, b)])]).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(maxdegree_1) :- mvpoli:maxdegree(y*x^3+y*x^5+x^8+y^2+z*x^5+z*y, 8).
+test(maxdegree_2) :- mvpoli:maxdegree(y^128+1, 128).
+test(mindegree_1) :- mvpoli:mindegree(y*x^3+y*x^5+x^8+y^2+z*x^5+z*y, 2).
+test(mindegree_2) :- mvpoli:mindegree(y^128+1, 0).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(polyplus_1) :-	mvpoli:polyplus(3*x, poly([m(4, 1, [v(1, x)])]), poly([m(7, 1, [v(1, x)])])).
+test(polyplus_2) :-	mvpoli:polyplus(3*x, 3*y, poly([m(3, 1, [v(1, x)]), m(3, 1, [v(1, y)])])).
+test(polyplus_3) :-	mvpoli:polyplus(poly([m(3, 1, [v(1, x)]), m(4, 1, [v(1, y)])]), 3*y, poly([m(3, 1, [v(1, x)]), m(7, 1, [v(1, y)])])).
+test(polyplus_4) :-	mvpoli:polyplus(3, 39, poly([m(42, 0, [])])).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(polyminus_1) :- mvpoli:polyminus(3*x, 4*x, poly([m(-1, 1, [v(1, x)])])).
+test(polyminus_2) :- mvpoli:polyminus(poly([m(3, 1, [v(1, x)])]), 4*x, poly([m(-1, 1, [v(1, x)])])).
+test(polyminus_3) :- mvpoli:polyminus(3*x, poly([m(4, 1, [v(1, x)])]), poly([m(-1, 1, [v(1, x)])])).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(polytimes_1) :- mvpoli:polytimes(x^3, x, poly([m(1, 4, [v(4, x)])])).
+test(polytimes_2) :- mvpoli:polytimes(x^3, poly([m(1, 8, [v(8, x)])]), poly([m(1, 11, [v(11, x)])])).
+test(polytimes_3) :- mvpoli:polytimes(poly([m(1, 1, [v(1, y)]), m(1, 3, [v(3, x)])]), x^5, poly([m(1, 6, [v(5, x), v(1, y)]), m(1, 8, [v(8, x)])])).
+test(polytimes_4) :- mvpoli:polytimes(x^3+y, x^5+y, poly([m(1, 2, [v(2, y)]), m(1, 4, [v(3, x), v(1, y)]), m(1, 6, [v(5, x), v(1, y)]), m(1, 8, [v(8, x)])])).
+test(polytimes_5) :-
+	mvpoli:as_polynomial(y*x^3+y*x^5+x^8+y^2+z*x^5+z*y, P),
+	mvpoli:polytimes(x^3+y+z, x^5+y, P).
+test(polytimes_6) :- mvpoli:polytimes(poly([]), poly([m(1, 3, [v(3, x)])]), poly([])).
+test(polytimes_7) :- mvpoli:polytimes(x^3, 0, poly([])).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(as_monomial_1) :- mvpoli:as_monomial(42, m(42, 0, [])).
+test(as_monomial_2) :- mvpoli:as_monomial(x, m(1, 1, [v(1, x)])).
+test(as_monomial_3) :- mvpoli:as_monomial(x^2, m(1, 2, [v(2, x)])).
+test(as_monomial_4) :- mvpoli:as_monomial(x^2*y^3, m(1, 5, [v(2, x), v(3, y)])).
+test(as_monomial_5) :- mvpoli:as_monomial(69*x^2*y^3, m(69, 5, [v(2, x), v(3, y)])).
+test(as_monomial_6) :- mvpoli:as_monomial(69*y^3*x^2*a^3, m(69, 8, [v(3, a), v(2, x), v(3, y)])).
+test(as_monomial_7) :- mvpoli:as_monomial(y^3*a^3*ab^3*a^6, m(1, 15, [v(9, a), v(3, ab), v(3, y)])).
+test(as_monomial_8) :- mvpoli:as_monomial(a*b*a^2*b^2, m(1, 6, [v(3, a), v(3, b)])).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(as_polynomial_1) :- mvpoli:as_polynomial(42, poly([m(42, 0, [])])).
+test(as_polynomial_2) :- mvpoli:as_polynomial(21 + 21, poly([m(42, 0, [])])).
+test(as_polynomial_3) :- mvpoli:as_polynomial(21 + x + 21 + x + x, poly([m(42, 0, []), m(3, 1, [v(1, x)])])).
+test(as_polynomial_4) :- mvpoli:as_polynomial(3*x - 4*x + 1, poly([m(1, 0, []), m(-1, 1, [v(1, x)])])).
+test(as_polynomial_5) :- mvpoli:as_polynomial(3*x - 4*x^2 + 1, poly([m(1, 0, []), m(3, 1, [v(1, x)]), m(-4, 2, [v(2, x)])])).
+test(as_polynomial_6) :- mvpoli:as_polynomial(x*y^2 + x^2*y + x^3, poly([m(1, 3, [v(1, x), v(2, y)]), m(1, 3, [v(2, x), v(1, y)]), m(1, 3, [v(3, x)])])).
+test(as_polynomial_7) :- mvpoli:as_polynomial(a*c+a^2+a*b+a, poly([m(1, 1, [v(1, a)]), m(1, 2, [v(1, a), v(1, b)]), m(1, 2, [v(1, a), v(1, c)]), m(1, 2, [v(2, a)])])).
+test(as_polynomial_8) :- mvpoli:as_polynomial(3*x-3*x, poly([])).
+test(as_polynomial_9) :- mvpoli:as_polynomial(3*x-y-3*x+y, poly([])).
+test(as_polynomial_10) :- mvpoli:as_polynomial(3*x-y-0-3*x+y, poly([])).
+%------------------------------------------------------------------------------
+%------------------------------------------------------------------------------
+test(polyval_1) :- mvpoli:polyval(y^3, [2], 8).
+test(polyval_1) :- mvpoli:polyval(x^2-y^2, [12, 12], 0).
+:- end_tests(mvpoli).
